a65100830e585579f89b590c72eaab2e7c5b7f69
nanahandresaputra <nanaggmu@gmail.com>
2025-09-11 18:22:08 +0700

    update new ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,9 @@
-import Navbar from "./components/navbar";
 import "./app.css";
-import Contact from "./components/contact";
-import Banner from "./components/banner";
-import Skills from "./components/skills";
-import Portofolios from "./components/portofolios";
-import AboutME from "./components/aboutme";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Banner />
-      <AboutME />
-      <Skills />
-      <Portofolios />
-      <Contact name="Contact" />
+      <p className="text-red-500">test</p>
     </div>
   );
 }
