1c965c4eca0b5bfd36d7c012357f2a7af19c3f4f
nanahandresaputra <nanaggmu@gmail.com>
2025-11-12 22:58:41 +0700

    add new porto

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,9 +7,10 @@ import { compProAssets } from "./porto-list/companyProfile";
 import { nnNextAiAssets } from "./porto-list/nnNextAi";
 import { gameBaseAssets } from "./porto-list/gameBase";
 import { goeApiAssets } from "./porto-list/goEventIdApi";
+import { goeCmsAssets } from "./porto-list/goEventIdCms";
 import darkBg from "./dark-bg.webp";
 import lightBg from "./light-bg.webp";
 import idImg from "./id.webp";
 import enImg from "./en.webp";
 
-export { nnprofile, radioStreamAssets, goeAppAssets, bannerGoe, bannerRadioStream, fquizAssets, netmoviesAssets, compProAssets, nnNextAiAssets, gameBaseAssets, goeApiAssets, darkBg, lightBg, enImg, idImg };
+export { nnprofile, radioStreamAssets, goeAppAssets, bannerGoe, goeCmsAssets, bannerRadioStream, fquizAssets, netmoviesAssets, compProAssets, nnNextAiAssets, gameBaseAssets, goeApiAssets, darkBg, lightBg, enImg, idImg };
---
+++ src/assets/porto-list/goEventIdCms/index.js
@@ -0,0 +1,12 @@
+import goeCms1 from "./1.webp";
+import goeCms2 from "./2.webp";
+import goeCms3 from "./3.webp";
+import goeCms4 from "./4.webp";
+import goeCms5 from "./5.webp";
+import goeCms6 from "./6.webp";
+import goeCms7 from "./7.webp";
+import goeCms9 from "./9.webp";
+
+const goeCmsAssets = [goeCms1, goeCms2, goeCms3, goeCms4, goeCms5, goeCms6, goeCms7, goeCms9];
+
+export { goeCmsAssets };
