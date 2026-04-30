d0b629b8355c815de722ddc8e9c53dfbf09405d8
nanahandresaputra <nanaggmu@gmail.com>
2026-04-30 10:50:34 +0700

    add google html

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import "./app.css";
 import Layout from "./layout";
 import HomePage from "./page/home";
 import PortoDetailPage from "./page/porto-detail";
+import SplashCursor from "./components/utils/splash-cursor";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="" element={<HomePage />} />
-          <Route path="porto-detail" element={<PortoDetailPage />} />
-        </Route>
-      </Routes>
-    </Router>
+    <section>
+      {/* <SplashCursor /> */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="" element={<HomePage />} />
+            <Route path="porto-detail" element={<PortoDetailPage />} />
+          </Route>
+        </Routes>
+      </Router>
+    </section>
   );
 }
 
